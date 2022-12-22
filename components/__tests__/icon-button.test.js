4d8b3141b04ad15addfa5f11303dab9f6c42e192
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-22 14:22:29 -0600

    Button test

--- components/__tests__/icon-button.test.js
+++ components/__tests__/icon-button.test.js
@@ -2,23 +2,39 @@
  * @jest-environment jsdom
  */
 
- import React from 'react'
- import { render, screen, fireEvent, getByText, getByLabelText } from '@testing-library/react'
- import '@testing-library/jest-dom'
- import userEvent from '@testing-library/user-event'
- 
- import IconButton from "../icon-button"
- 
- const user = userEvent.setup()
- 
+import React from 'react';
+import { render, screen, fireEvent, getByText, getByLabelText } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+
+import IconButton from '../icon-button';
+
+const user = userEvent.setup();
 
 describe('IconButton', () => {
-    it('labels the button', () => {
+  it('labels the button', () => {
     const textFixture = 'Send it!';
     const { getByLabelText } = render(<IconButton name={textFixture} />);
 
     const buttonText = getByLabelText(textFixture);
 
     expect(buttonText).toBeInTheDocument();
-  })
-})
+  });
+  it('can be reached and operated with the keyboard', async () => {
+    let clicked = false;
+    render(
+      <IconButton
+        name="Daisy"
+        onClick={() => {
+          clicked = true;
+        }}
+      />
+    );
+    const button = screen.getByRole('button');
+
+    button.focus();
+    await user.keyboard('[Enter]');
+
+    expect(clicked).toBe(true);
+  });
+});
