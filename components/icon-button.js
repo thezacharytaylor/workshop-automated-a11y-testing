f401f4cee79aa778697e440ed33f23e8f37f757a
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-21 19:16:32 -0600

    Fixing the button per challenge

--- components/icon-button.js
+++ components/icon-button.js
@@ -1,15 +1,18 @@
-import React from "react"
+import React from 'react';
 
-const IconButton = ({name, onClick}) => {
-    return (
-        <div
-            className="btn-submit btn-lookingglass"
-            data-testid="btn-submit"
-            onClick={(event) => { onClick(event)}}
-        >
-            <span className="icon-lookingglass-white"></span>
-        </div>
-    )
-}
+const IconButton = ({ name, onClick }) => {
+  return (
+    <button
+      aria-label={name}
+      className="btn-submit btn-lookingglass"
+      data-testid="btn-submit"
+      onClick={(event) => {
+        onClick(event);
+      }}
+    >
+      <span className="icon-lookingglass-white">{name}</span>
+    </button>
+  );
+};
 
-export default IconButton
+export default IconButton;
