a37683ef251880a88d3eb17e77cc4a3a2c052d2f
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-23 13:43:02 -0600

    cypress: reaching submenu items w/ kb

--- exercise2-cypress-components/meganav.cy.js
+++ exercise2-cypress-components/meganav.cy.js
@@ -9,3 +9,24 @@ it('should operate with the keyboard via toggle buttons', () => {
 
   cy.focused().should('have.attr', 'aria-expanded', 'true');
 });
+
+it('submenus should not be reachable when closed', () => {
+  cy.mount(<MegaNav />);
+  cy.get('button[data-testid="megamenu-section1"]').focus().realPress('Tab');
+
+  cy.focused().should('not.have.attr', '[data-testid="link-0"]');
+
+  cy.focused().then(($el) => {
+    expect($el).to.have.attr('data-testid', 'megamenu-section2');
+  });
+});
+
+it('reach submenu items with keyboard', () => {
+  cy.mount(<MegaNav />);
+  cy.get('[data-testid="megamenu-section1"]').focus().click().realPress('Tab');
+  cy.focused().should('not.have.attr', '[data-testid="megamenu-section2"]');
+
+  cy.focused().then(($el) => {
+    expect($el).to.have.attr('data-testid', 'link-0');
+  });
+});
