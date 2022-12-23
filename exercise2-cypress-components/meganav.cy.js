3c201f9bf83668abf8946b7bcbac2ae47f231546
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-23 14:09:12 -0600

    Adding describe back, but with Cypress 11 syntax

--- exercise2-cypress-components/meganav.cy.js
+++ exercise2-cypress-components/meganav.cy.js
@@ -3,30 +3,32 @@ import { mount } from '@cypress/react';
 
 import MegaNav from '../components/meganav';
 
-it('should operate with the keyboard via toggle buttons', () => {
-  cy.mount(<MegaNav />);
-  cy.get('[data-testid="megamenu-section1"]').focus().click();
+describe('MegaNav', () => {
+  it('should operate with the keyboard via toggle buttons', () => {
+    cy.mount(<MegaNav />);
+    cy.get('[data-testid="megamenu-section1"]').focus().click();
 
-  cy.focused().should('have.attr', 'aria-expanded', 'true');
-});
-
-it('submenus should not be reachable when closed', () => {
-  cy.mount(<MegaNav />);
-  cy.get('button[data-testid="megamenu-section1"]').focus().realPress('Tab');
-
-  cy.focused().should('not.have.attr', '[data-testid="link-0"]');
-
-  cy.focused().then(($el) => {
-    expect($el).to.have.attr('data-testid', 'megamenu-section2');
+    cy.focused().should('have.attr', 'aria-expanded', 'true');
   });
-});
 
-it('reach submenu items with keyboard', () => {
-  cy.mount(<MegaNav />);
-  cy.get('[data-testid="megamenu-section1"]').focus().click().realPress('Tab');
-  cy.focused().should('not.have.attr', '[data-testid="megamenu-section2"]');
+  it('submenus should not be reachable when closed', () => {
+    cy.mount(<MegaNav />);
+    cy.get('button[data-testid="megamenu-section1"]').focus().realPress('Tab');
 
-  cy.focused().then(($el) => {
-    expect($el).to.have.attr('data-testid', 'link-0');
+    cy.focused().should('not.have.attr', '[data-testid="link-0"]');
+
+    cy.focused().then(($el) => {
+      expect($el).to.have.attr('data-testid', 'megamenu-section2');
+    });
+  });
+
+  it('reach submenu items with keyboard', () => {
+    cy.mount(<MegaNav />);
+    cy.get('[data-testid="megamenu-section1"]').focus().click().realPress('Tab');
+    cy.focused().should('not.have.attr', '[data-testid="megamenu-section2"]');
+
+    cy.focused().then(($el) => {
+      expect($el).to.have.attr('data-testid', 'link-0');
+    });
   });
 });
