5d68d772aea8a7d2f2d54b91f305faa11d4b8fc7
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-23 18:32:28 -0600

    cypress: payment module test

--- exercise3-cypress-integration/PassesPage.test.js
+++ exercise3-cypress-integration/PassesPage.test.js
@@ -1,12 +1,19 @@
 describe('Passes Page', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:1234/passes')
-        
-        cy.injectAxe()
-    })
-    it('should have no accessibility violations on load', () => {
-        cy.checkA11y({
-            exclude: ['div#portal-root']
-        })
-    })
-})
+  beforeEach(() => {
+    cy.visit('http://localhost:1234/passes');
+
+    cy.injectAxe();
+  });
+  xit('should have no accessibility violations on load', () => {
+    cy.checkA11y({
+      exclude: ['div#portal-root'],
+    });
+  });
+  it('focus should return to previous element on modal close', () => {
+    cy.get('#btn-join-pro').focus().click().realPress('Tab');
+    cy.focused().click();
+    cy.focused().then(($el) => {
+      expect($el).to.have.attr('id', 'btn-join-pro');
+    });
+  });
+});
