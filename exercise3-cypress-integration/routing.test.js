454e94a18501580b9457dc081c0eaa64082a5841
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-23 21:06:42 -0600

    cypress: Skeleton for route tests

---
+++ exercise3-cypress-integration/routing.test.js
@@ -0,0 +1,11 @@
+describe('Passes Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:1234/');
+
+    // cy.injectAxe();
+  });
+
+  it('title changes when navigating pages.', () => {});
+
+  it('keyboard users can navigate pages.', () => {});
+});
